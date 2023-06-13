b23f1bc8e21e17249d61c17863917c2f564b3a5a
Jonthan McAvinue <mcavinuejon@gmail.com>
2023-06-13 15:15:47 -0400

    Adding barebones introduction and classes to program

---
+++ MangaCollectionLibrary/MangaCollectionLibrary/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MangaCollectionLibrary
+{
+    public abstract class Menu
+    {
+        public void MainMenu()
+        {
+            Console.Title = "Manga Collection Record";
+
+        }
+    }
+}
